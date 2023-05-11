552de4fd544d3f027a31bae7378587a9bd61a079
Carlos Ruiz <18002171@alumnos.utleon.edu.mx>
2023-05-11 14:27:54 -0600

    Agregar archivos de proyecto.

---
+++ IDGS902_Tema1/Controllers/NewController.cs
@@ -0,0 +1,72 @@
+﻿using IDGS902_Tema1.Models;
+using IDGS902_Tema1.Services;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDGS902_Tema1.Controllers
+{
+    public class NewController : Controller
+    {
+        public ActionResult NewIndex()
+        {
+            ViewBag.Message = "Hola mundo esta es una nueva interfaz de index";
+
+            return View();
+        }
+
+        public ActionResult OperasBas(string n1,string n2,string ope)
+        {
+            int res = 0;
+            if (ope == "suma")
+            {
+                res = Convert.ToInt16(n1) + Convert.ToInt16(n2);
+            }
+            if (ope == "resta")
+            {
+                res = Convert.ToInt16(n1) - Convert.ToInt16(n2);
+            }
+            if (ope == "multiplicacion")
+            {
+                res = Convert.ToInt16(n1) * Convert.ToInt16(n2);
+            }
+            if (ope == "divicion")
+            {
+                res = Convert.ToInt16(n1) / Convert.ToInt16(n2);
+            }
+            ViewBag.Res = Convert.ToString(res);
+
+            return View();
+        }
+        public ActionResult OperasBas2(Calculos op)
+        {
+            var model = new Calculos();
+            model.Res = op.Num1 + op.Num2;
+            ViewBag.Res = model.Res;
+
+            return View(model);
+        }
+        public ActionResult OperasBas3(NewCalculos op)
+        {
+            var model = new NewCalculos();
+            model.DistanciaDeDosPuntos(op.X1,op.X2,op.Y1,op.Y2);
+            return View(model);
+        }
+        public ActionResult MuestraPulques()
+        {
+            var pulques1=new PulquesServices();
+            var model = pulques1.ObtenerPulque();
+            return View(model);
+        }
+        public ActionResult MuestraPulques2()
+        {
+            var pulques1 = new PulquesServices();
+            var model = pulques1.ObtenerPulque();
+            return View(model);
+        }
+    }
+}
---
+++ IDGS902_Tema1/Models/NewCalculos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Web;
+
+namespace IDGS902_Tema1.Models
+{
+    public class NewCalculos
+    {
+        public int X1 { get; set; }
+        public int X2 { get; set; }
+        public int Y1 { get; set; }
+        public int Y2 { get; set; }
+        public int Res { get; set; }
+        public void DistanciaDeDosPuntos(int X1,int X2,int Y1,int Y2)
+        {
+            
+            this.Res= (int)Math.Sqrt(Math.Pow(Convert.ToDouble(X2 - X1), 2.0) + Math.Pow(Convert.ToDouble(Y2 - Y1), 2.0));
+
+        }
+    }
+}
